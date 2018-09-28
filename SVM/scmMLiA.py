68a25d3206c842f140b3ead639b15c366bfb83be
luluhan123 <lulu_han@aliyun.com>
2018-09-28 21:51:53 +0800

    Add files via upload

---
+++ SVM/scmMLiA.py
@@ -0,0 +1,86 @@
+from numpy import *
+from time import sleep
+
+#辅助函数一：加载类标签和数据矩阵
+def loadDataSet(fileName):
+    dataMat = []
+    labelMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        lineArr = line.strip().split('\t')
+        dataMat.append([float(lineArr[0]),float(lineArr[1])])
+        labelMat.append(float(lineArr[2]))
+    return dataMat,labelMat
+
+#辅助函数二：只要函数值不等于输入值i，就会随机选择第一个alpha的下标，m是所有alpha的数目，也就是样本点的数目
+def selectJrand(i,m):
+    j = i
+    while(j==i):
+        j = int(random.uniform(0,m))
+    return j
+
+#辅助函数三：调整大于H或小于L的alpha的值
+def clipAlpha(aj,H,L):
+    if aj>H:
+        aj=H
+    if L>aj:
+        aj=L
+    return aj
+
+def smoSimple(dataMatIn,classLabels,C,toler,maxIter):
+    dataMatrix = mat(dataMatIn)
+    labelMat = mat(classLabels).transpose()
+    b = 0
+    m,n = shape(dataMatrix)
+    alphas = mat(zeros((m,1)))
+    iter = 0
+    while(iter<maxIter):
+        alphaPairsChanged = 0
+        for i in range(m):
+            fxi = float(multiply(alphas,labelMat).T * (dataMatrix * dataMatrix[i,:].T)) + b
+            Ei = fxi - float(labelMat[i])
+            if((labelMat[i] * Ei < -toler) and (alphas[i] < C)) or \
+                    ((labelMat[i] * Ei > toler) and (alphas[i]>0)):
+                j = selectJrand(i,m)
+                fxj = float(multiply(alphas, labelMat).T * (dataMatrix * dataMatrix[j, :].T)) + b
+                Ej = fxj - float(labelMat[j])
+                alphaIold = alphas[i].copy()
+                alphaJold = alphas[j].copy()
+                if(labelMat[i] != labelMat[j]):
+                    L = max(0,alphas[j] - alphas[i])
+                    H = min(C,C + alphas[j] - alphas[i])
+                else:
+                    L = max(0,alphas[j] + alphas[i] - C)
+                    H = min(C,alphas[j] + alphas[i])
+                if L==H:
+                    print("L==H")
+                    continue
+                eta = 2.0 * dataMatrix[i,:] * dataMatrix[j,:].T - \
+                    dataMatrix[i,:] * dataMatrix[i,:].T - \
+                    dataMatrix[j,:] * dataMatrix[j,:].T
+                if(eta >= 0):
+                    print("eta>=0")
+                    continue
+                alphas[j] -= labelMat[j]*(Ei-Ej)/eta
+                alphas[j] = clipAlpha(alphas[j],H,L)
+                if(abs(alphas[j] - alphaJold) < 0.00001 ):
+                    print("j not moving enough")
+                    continue
+                alphas[i] += labelMat[j] * labelMat[i] * (alphaJold - alphas[j])
+                # labelNatri对应y,alphas对应a_new，dataMatrix[i,:]*dataMatrix[i,:].T对应Kii
+                b1 = b - Ei - labelMat[i]*(alphas[i] - alphaIold) * dataMatrix[i,:]*dataMatrix[i,:].T - \
+                    labelMat[j]*(alphas[j]-alphaJold) * dataMatrix[i,:]*dataMatrix[j,:].T
+                b2 = b - Ej - labelMat[i] * (alphas[i] - alphaIold) * dataMatrix[i, :] * dataMatrix[j, :].T - \
+                     labelMat[j] * (alphas[j] - alphaJold) * dataMatrix[j, :] * dataMatrix[j, :].T
+                if(0 < alphas[i]) and (C > alphas[i]):
+                    b = b1
+                elif(0 < [j]) and (C > alphas[j]):
+                    b = b2
+                else:
+                    b = (b1 + b2) / 2.0
+                alphaPairsChanged += 1
+                print()
+
+if __name__ == '__main__':
+    dataArr,labelArr = loadDataSet('testSet.txt')
+    print(labelArr)
